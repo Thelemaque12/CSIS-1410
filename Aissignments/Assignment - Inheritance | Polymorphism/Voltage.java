1c159aec941da891fc0a5edc5a939d35278b82e6
Jefson Simeus <61072746+Thelemaque12@users.noreply.github.com>
2022-02-19 22:09:52 -0500

    Create Voltage.java

---
+++ Aissignments/Assignment - Inheritance | Polymorphism/Voltage.java
@@ -0,0 +1,14 @@
+package phones;
+
+/**
+ * Initializes three values
+ * related to the voltage of 
+ * RotaryPhone & DeskPhone.
+ * 
+ * @author Jefson S
+ *
+ */
+	public enum Voltage{
+		V110, V220, DUAL
+	}
+
